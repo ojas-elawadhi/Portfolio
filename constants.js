a3ac83f4c03c7ef42e88fb9ed81b75b677a7412e
Ojas Elawadhi <ojaselawadhi1@gmail.com>
2023-11-29 01:27:34 +0530

    preview image change

--- constants.js
+++ constants.js
@@ -17,7 +17,7 @@ export const METADATA = {
     "Folio",
   ].join(", "),
   image:
-    "https://res.cloudinary.com/dywdhyojt/image/upload/v1690572126/preview.png",
+    "https://res.cloudinary.com/dhjioxmiq/image/upload/v1701201348/preview_umu5ry.png",
   language: "English",
 };
 
